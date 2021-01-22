19410aeb5f0149467c51cb35e1ca086ec0bf8744
Almost Engineer <bitsecondal@gmail.com>
2021-01-21 23:06:34 -0600

    Updated test cases

---
+++ Almostengr.Tuscblackchamber.Tests/BaseTest.cs
@@ -0,0 +1,41 @@
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Almostengr.Tuscblackchamber.Tests
+{
+    public class BaseTest
+    {
+        public IWebDriver StartBrowser()
+        {
+            IWebDriver driver = null;
+            ChromeOptions options = new ChromeOptions();
+
+#if RELEASE
+            options.AddArgument("--headless");
+#endif
+
+            driver = new ChromeDriver(options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+
+            return driver;
+        }
+
+        public void GoHome(IWebDriver driver)
+        {
+#if RELEASE
+            driver.Navigate().GoToUrl("http://tuscblackchamber.org");
+#else
+            driver.Navigate().GoToUrl("http://192.168.57.117:8080");
+#endif
+        }
+
+        public void CloseBrowser(IWebDriver driver)
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
---
+++ Almostengr.Tuscblackchamber.Tests/ContactTests.cs
@@ -0,0 +1,47 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Almostengr.Tuscblackchamber.Tests
+{
+    public class ContactTests : BaseTest
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = StartBrowser();
+        }
+
+        [Test]
+        public void ProcessValidContactSubmission()
+        {
+            GoHome(driver);
+
+            driver.FindElement(By.LinkText("CONTACT")).Click();
+
+            IWebElement customerFirst = driver.FindElement(By.Name("customerfirst"));
+            customerFirst.SendKeys("Tuscaloosa");
+
+            driver.FindElement(By.Name("customerlast")).SendKeys("Tester");
+            driver.FindElement(By.Name("phonenumber")).SendKeys("205-555-1234");
+
+            string repsonseMsg = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam feugiat nunc et turpis bibendum tempus. Aliquam finibus euismod aliquam. Morbi semper lectus a mauris commodo egestas sit amet nec risus.";
+            driver.FindElement(By.Name("cresponse")).SendKeys(repsonseMsg);
+
+            customerFirst.Submit();
+
+            driver.FindElement(By.Id("successmessage"));
+
+            // assert
+            Assert.Pass();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            CloseBrowser(driver);
+        }
+    }
+}
