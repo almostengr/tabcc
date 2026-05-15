f80610e02d4d757cfa69a08b14a90b88f2920ab1
almostengr <info@rhtservices.net>
2026-05-14 21:44:12 -0500

    closed #19 added firebase configuration; closed #14 updated membership application

--- mkdocwebsite/theme/js/javascript.js
+++
@@ -1,9 +0,0 @@
-function externalLinks() {
-    var anchors = document.getElementsByTagName('a');
-    for (var i = 0; i < anchors.length; i++) {
-        if (anchors[i].getAttribute("href").startsWith("http")) {
-            anchors[i].target = "_blank";
-        }
-    }
-}
-externalLinks();
